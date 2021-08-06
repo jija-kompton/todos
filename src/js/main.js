6b939821136a9c319952dcc887f5b31cba8a9300
jija-kompton <88193917+jija-kompton@users.noreply.github.com>
2021-08-06 17:32:36 +0400

    Add modal and lo

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,31 @@
 import '../scss/main.scss';
+
+const CLASS_LIST = {
+    MODAL: 'modal',
+    MODAL_ACTIVE: 'modal--active',
+    TRIGGER_OPEN: 'js-modal-open',
+    TRIGGER_CLOSE: 'js-modal-close'
+};
+
+document.addEventListener('click', (event) =>{
+
+    if (
+        event.target.closest(`.${CLASS_LIST.TRIGGER_OPEN}`)) {
+            event.preventDefault();
+
+            const target = event.target.closest(`.${CLASS_LIST.TRIGGER_OPEN}`);
+            const modal = document.querySelector('.modal');
+
+            modal.classList.add(CLASS_LIST.MODAL_ACTIVE);
+        }
+        
+    if (event.target.closest(`.${CLASS_LIST.TRIGGER_CLOSE}`) ||
+        (event.target.classList.contains(CLASS_LIST.MODAL_ACTIVE))
+    ) {
+        event.preventDefault();
+
+        const modal = event.target.closest(`.${CLASS_LIST.MODAL_ACTIVE}`);
+
+        modal.classList.remove(CLASS_LIST.MODAL_ACTIVE);
+    }
+});
